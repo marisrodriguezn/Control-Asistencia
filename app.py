5cbdbcd3b9849b25490310709e0dc7851e3c1a08
marisrodriguezn <marisrodriguezn@gmail.com>
2025-04-23 13:06:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ st.title("📋 Registro de Asistencia por QR")
 
 # 📂 Cargar el Excel directamente desde GitHub (reemplaza con tu enlace RAW)
 excel_url = "https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fraw.githubusercontent.com%2Fmarisrodriguezn%2FControl-Asistencia%2Frefs%2Fheads%2Fmain%2FInvitados_con_QR.xlsx&wdOrigin=BROWSELINK"
-
 try:
     archivo_excel = BytesIO(requests.get(excel_url).content)
     df = pd.read_excel(archivo_excel)
